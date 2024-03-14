3e6802797485e9ea14e18eb4077e6afb8b34e320
Alexandre Vasconcellos <avasconcellos204@gmail.com>
2024-03-14 11:29:11 -0300

    fix request ID typo

--- ec2.js
+++ ec2.js
@@ -32,13 +32,13 @@ async function processMessages() {
                 console.log('Processing message:', body);
                 const processedOutput = processNumber(body.number)
 
-                console.log('processed: ', processedOutput.toString(), body.request_id.toString() )
+                console.log('processed: ', processedOutput.toString(), body.requestId.toString() )
                 console.log('updating ', DYNAMO_TABLE)
                 // Update DynamoDB table                
                 await dynamoDB.updateItem({
                     TableName: DYNAMO_TABLE,
                     Key: {
-                        'request_id': { N: body.request_id.toString() }
+                        'request_id': { N: body.requestId.toString() }
                     },
                     UpdateExpression: 'SET #output = :attrValue, #attrName2 = :attrValue2',
                     ExpressionAttributeNames: {
